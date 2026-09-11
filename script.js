7a7483357ccd5cd4a5437025e7e2eaa46dfb13ce
K SABA <sabacr745@gmail.com>
2026-09-11 16:49:07 +0530

    base

---
+++ script.js
@@ -0,0 +1,53 @@
+//dark mmode function
+/*-------------------------------------------------------*/
+function changecolor(){
+    document.body.style.backgroundColor=" rgba(0, 0, 0, 0.87)";
+    document.body.style.color="gold";
+let name= document.querySelector(".name");
+name.style.backgroundColor="grey";
+name.style.color="gold";
+
+let b1 = document.querySelector(".b1");
+b1.style.backgroundColor="grey";
+b1.style.color="gold";
+
+let b2 = document.querySelector(".b2");
+b2.style.backgroundColor="grey";
+b2.style.color="gold";
+
+let b3 = document.querySelector(".b3");
+b3.style.backgroundColor="grey";
+b3.style.color="gold";
+
+let b4 = document.querySelector(".b4");
+b4.style.backgroundColor="grey";
+b4.style.color="gold";
+
+let darkmode = document.querySelector("#darkmode");
+darkmode.style.backgroundColor="grey";
+darkmode.style.color="gold";
+
+let about = document.querySelector("#about");
+about.style.backgroundColor="grey";
+
+let project1 = document.querySelector(".project1");
+project1 .style.backgroundColor="grey";
+
+let project2 = document.querySelector(".project2");
+project2 .style.backgroundColor="grey";
+
+let project3 = document.querySelector(".project3");
+project3 .style.backgroundColor="grey";
+
+let contact = document.querySelector("#contact");
+contact.style.backgroundColor="grey";
+
+let footer = document.querySelector(".footer");
+footer.style.backgroundColor="grey";
+footer.style.color="gold";
+
+let SocialMedia = document.querySelector("#SocialMedia");
+SocialMedia.style.backgroundColor="grey";
+
+}
+/*-------------------------------------------------------*/
